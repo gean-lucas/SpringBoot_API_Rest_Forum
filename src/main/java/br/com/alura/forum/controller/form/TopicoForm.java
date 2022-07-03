e61d56f53bcbe0dd2d3647d08409dab2a2694620
gean-lucas <geanfarias@on.br>
2022-07-03 13:48:53 -0300

    api pronta

---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,51 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+	@NotBlank @Length(min = 5)
+	private String titulo;
+	@NotNull @NotEmpty @Length(min = 10)
+	private String mensagem;
+	@NotNull @NotEmpty
+	private String nomeCurso;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public Topico converter(CursoRepository cursorepository) {
+		Curso curso = cursorepository.findByNome(this.nomeCurso);
+		return new Topico(this.titulo, this.mensagem, curso);
+	}
+
+}
